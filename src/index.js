69c3a346d199cac83409773bc58f7a125e729811
Guilherme Sposito <guilhermesposito14@gmail.com>
2023-10-17 18:38:43 -0300

    Adicionando Algumas variáveis de ambiente

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const routers = require('./routers/router')
+require('dotenv').config({ path: './.env' })
 
 app.use(express.json())
 app.use(routers)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Servidor iniciado")
 })
