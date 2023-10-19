357b556fb49e705e7e940dbcc2e7fd1e33d9ec8c
Guilherme Sposito <guilhermesposito14@gmail.com>
2023-10-19 09:21:46 -0300

    Concertando envio de amils

--- src/controllers/envioemail.js
+++ src/controllers/envioemail.js
@@ -3,17 +3,22 @@ const transport = require('../utils/conexaoemail')
 
 const envioDeEmail = async (req, res) => {
     try {
+        const { usuario } = req
+        const { to, titulo, mensagem } = req.body
 
+        if (!to || !titulo || !mensagem) {
+            return res.status.json({ mensagem: "Deve ser informado campos obrigatórios!" })
+        }
 
         const info = await transport.sendMail({
-            from: '"Guilherme Sposito" <gulhermesposito14@gmail.com>', // sender address
-            to: "gui-calandrin@hotmail.com", // list of receivers
-            subject: "Teste De Envio De Emails", // Subject line
-            html: "<h1>Teste de Envio de emails bem sucedido</h1>", // html body
+            from: `"${usuario.nome}" <${usuario.email}>`, // sender address
+            to: `${to}`, // list of receivers
+            subject: `${titulo}`, // Subject line
+            text: `${mensagem}` // html body
         });
 
 
-        res.status(200).json({ mensagem: "Email enviado com sucesso!" })
+        res.status(200).json({ mensagem: `Email de ${usuario.nome} enviado com sucesso para o email: ${to}` })
     } catch (error) {
         res.status(500).json({ mensagem: "Erro NO SERVIDOR" })
     }
